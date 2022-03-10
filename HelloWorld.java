73302dde78ea450dab462346c73994fef961c1a5
Student1202 <silinskij.nl@gmail.com>
2022-03-10 20:32:31 +0700

    add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+
+    public static void main(String []args){
+       System.out.println("Hello World");
+       int num = 10;
+       System.out.println(num);
+       String str = "String str";
+       System.out.println(str);
+       Scanner input = new Scanner(System.in);
+       String str2 = input.nextLine();
+       System.out.println(str2);
+    }
+}
